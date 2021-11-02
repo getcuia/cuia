5d273b25177f00a1877707f0cc5e4d10583eee93
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-11-02 19:20:46 -0300

    Add basic ANSI escape sequence manipulation

--- src/kay/ansi/__init__.py
+++ src/kay/ansi/__init__.py
@@ -1,1 +1,16 @@
 """Simple utilities for working with ANSI escape sequences."""
+
+
+def esc(code: str):
+    """
+    Return an ANSI escape sequence for the given code.
+
+    Idea taken from [here](https://realpython.com/lessons/ansi-escape-sequences/).
+    """
+    return f"\033[{code}m"
+
+
+if __name__ == "__main__":
+    print("this is ", esc("31"), "really", esc(0), " important", sep="")
+    print("this is ", esc("31;1"), "really", esc(0), " important", sep="")
+    print("this is ", esc("31;1;4"), "really", esc(0), " important", sep="")
