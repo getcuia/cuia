5f4ec23ea0b415b678398464b1ce6c5a8ce74151
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-11-02 18:58:23 -0300

    Add some dummy code in Program.start for now

--- src/kay/program.py
+++ src/kay/program.py
@@ -1,11 +1,15 @@
 """Program class."""
 
 
+from __future__ import annotations
+
 import sys
 from dataclasses import dataclass
 from queue import Queue
 from typing import TextIO
 
+from kay.command import Command
+from kay.event import Event
 from kay.model import Model
 
 
@@ -14,11 +18,15 @@ class Program:
     """Program class."""
 
     model: Model
-    events: Queue = Queue()
+    events: Queue[Event] = Queue()
     output: TextIO = sys.stdout
     input: TextIO = sys.stdin
 
     def start(self):
         """Start program."""
+        commands: Queue[Command] = Queue()
         if (init_cmd := self.model.init()) is not None:
-            self.events.put(init_cmd)
+            commands.put(init_cmd)
+
+        sys.stdout.write(self.model.view())
+        print(sys.stdin.read())
