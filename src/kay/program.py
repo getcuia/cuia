509fd166d566166960c0e72699217b6a4fc94a4f
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-11-04 13:29:58 -0300

    Add a start method sketch

--- src/kay/program.py
+++ src/kay/program.py
@@ -2,11 +2,12 @@
 
 
 from __future__ import annotations
+import asyncio
 
-import sys
+import curses
+from asyncio import Queue
 from dataclasses import dataclass
-from queue import Queue
-from typing import TextIO
+from typing import Optional
 
 from kay.command import Command
 from kay.message import Message
@@ -18,15 +19,55 @@ class Program:
     """Program class."""
 
     model: Model
-    messages: Queue[Message] = Queue()
-    output: TextIO = sys.stdout
-    input: TextIO = sys.stdin
+    messages: Optional[Queue[Message]] = None
+    # output: TextIO = sys.stdout
+    # input: TextIO = sys.stdin
 
-    def start(self):
+    async def start(self):
         """Start program."""
+        # Very similar to curses.wrapper
+        try:
+            stdscr = curses.initscr()
+            curses.noecho()
+            # curses.cbreak()
+            stdscr.keypad(True)
+            stdscr.nodelay(True)
+            # try:
+            #     curses.start_color()
+            # except:
+            #     pass
+
+            return await self._start_impl(stdscr)
+        finally:
+            if "stdscr" in locals():
+                stdscr.keypad(False)
+                curses.echo()
+                curses.nocbreak()
+                curses.endwin()
+
+    async def _start_impl(self, win: curses._CursesWindow):
+        """Start program implementation."""
+        # Queues have to be created inside the coroutine's event loop
+        self.messages = Queue()
         commands: Queue[Command] = Queue()
         if (init_cmd := self.model.init()) is not None:
-            commands.put(init_cmd)
+            await commands.put(init_cmd)
 
-        sys.stdout.write(self.model.view())
-        print(sys.stdin.read())
+        while True:
+            win.erase()
+            win.addstr(self.model.view())
+            win.refresh()
+            await asyncio.sleep(1 / 60)
+
+            # FIX: repeated from the beginning of the method
+            if (message := Message.from_curses(win)) is not None:
+                await self.messages.put(message)
+
+            try:
+                message = self.messages.get_nowait()
+            except asyncio.QueueEmpty:
+                continue
+
+            # FIX: repeated from the beginning of the method
+            if (cmd := self.model.update(message)) is not None:
+                await commands.put(cmd)
