a03199e1e31de3dc765a5f280300b22c0a012d6a
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-11-08 20:47:01 -0300

    Remove the WCAG 2.0 luminance implementation

--- src/kay/color.py
+++ src/kay/color.py
@@ -320,40 +320,6 @@ class Color(NamedTuple):
         """
         return not self.islight()
 
-    def luminance(self) -> float:
-        """
-        Return the luminance of this color.
-
-        The luminance is a value between 0.0 and 1.0.
-
-        Source: [Web Content Accessibility Guidelines (Version 2.0)](http://www.w3.org/TR/2008/REC-WCAG20-20081211/#relativeluminancedef).
-
-        Examples
-        --------
-        >>> Color(0, 0, 0).luminance()
-        0.0
-        >>> Color.frombytes(23, 23, 23).luminance()  # doctest: +NUMBER
-        0.0085681256
-        >>> Color.frombytes(255, 255, 255).luminance()
-        1.0
-        """
-        r = (
-            self.red / 12.92
-            if self.red <= 0.03928
-            else ((self.red + 0.055) / 1.055) ** 2.4
-        )
-        g = (
-            self.green / 12.92
-            if self.green <= 0.03928
-            else ((self.green + 0.055) / 1.055) ** 2.4
-        )
-        b = (
-            self.blue / 12.92
-            if self.blue <= 0.03928
-            else ((self.blue + 0.055) / 1.055) ** 2.4
-        )
-        return 0.2126 * r + 0.7152 * g + 0.0722 * b
-
 
 # The following colors were taken as the average of the colors found in
 # <https://en.wikipedia.org/wiki/ANSI_escape_code#3-bit_and_4-bit>.
