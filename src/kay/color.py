f8373a70969ffaf9bcfba6de826d70fdf1a25e6f
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-11-08 22:09:37 -0300

    Adjust lightness of the default colors

--- src/kay/color.py
+++ src/kay/color.py
@@ -405,18 +405,6 @@ class Color(NamedTuple):
         return not self.islight()
 
 
-# The following colors were taken as the average of the colors found in
-# <https://en.wikipedia.org/wiki/ANSI_escape_code#3-bit_and_4-bit>.
-BLACK = Color.frombytes(1, 1, 1)  # Color.frombytes(0, 0, 0)
-RED = Color.frombytes(179, 16, 14)  # Color.frombytes(173, 0, 0)
-GREEN = Color.frombytes(11, 172, 22)  # Color.frombytes(0, 173, 0)
-YELLOW = Color.frombytes(203, 176, 27)  # Color.frombytes(173, 173, 0)
-BLUE = Color.frombytes(10, 30, 186)  # Color.frombytes(0, 0, 173)
-MAGENTA = Color.frombytes(155, 20, 164)  # Color.frombytes(173, 0, 173)
-CYAN = Color.frombytes(34, 150, 184)  # Color.frombytes(0, 173, 173)
-WHITE = Color.frombytes(204, 205, 205)  # Color.frombytes(173, 173, 173)
-
-
 @dataclass(frozen=True)
 class Foreground:
     """A foreground color."""
@@ -545,3 +533,34 @@ def lch_to_luv(ell: float, c: float, h: float) -> tuple[float, float, float]:
     u = c * math.cos(h)
     v = c * math.sin(h)
     return ell, u, v
+
+
+# The following colors were taken as the average of the colors found in
+# <https://en.wikipedia.org/wiki/ANSI_escape_code#3-bit_and_4-bit>.
+# They had then their lightness adjusted to be closer to the average of the
+# colors in the table.
+LIGHTNESS = 0.25
+BLACK = Color.frombytes(1, 1, 1).with_lightness(
+    LIGHTNESS ** 4
+)  # Color.frombytes(0, 0, 0)
+RED = Color.frombytes(179, 16, 14).with_lightness(
+    LIGHTNESS
+)  # Color.frombytes(173, 0, 0)
+GREEN = Color.frombytes(11, 172, 22).with_lightness(
+    LIGHTNESS
+)  # Color.frombytes(0, 173, 0)
+YELLOW = Color.frombytes(203, 176, 27).with_lightness(
+    LIGHTNESS
+)  # Color.frombytes(173, 173, 0)
+BLUE = Color.frombytes(10, 30, 186).with_lightness(
+    LIGHTNESS
+)  # Color.frombytes(0, 0, 173)
+MAGENTA = Color.frombytes(155, 20, 164).with_lightness(
+    LIGHTNESS
+)  # Color.frombytes(173, 0, 173)
+CYAN = Color.frombytes(34, 150, 184).with_lightness(
+    LIGHTNESS
+)  # Color.frombytes(0, 173, 173)
+WHITE = Color.frombytes(204, 205, 205).with_lightness(
+    1 - LIGHTNESS ** 4
+)  # Color.frombytes(173, 173, 173)
