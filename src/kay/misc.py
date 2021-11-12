75bea9ab3f3990650920c7bd7b261013c155c671
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-11-12 18:28:12 -0300

    Add a doctest to clamp

--- src/kay/misc.py
+++ src/kay/misc.py
@@ -5,7 +5,18 @@ from typing import Iterable, Pattern, Text
 
 
 def clamp(value: float, min_value: float = 0.0, max_value: float = 1.0) -> float:
-    """Clamp a value to a range."""
+    """
+    Clamp a value to a range.
+
+    Examples
+    --------
+    >>> clamp(0.5)
+    0.5
+    >>> clamp(-0.5)
+    0.0
+    >>> clamp(1.5)
+    1.0
+    """
     return max(min_value, min(value, max_value))
 
 
