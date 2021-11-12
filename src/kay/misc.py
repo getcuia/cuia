8a50ad418bfc94f361b05b0b4fac975c75d4b7c3
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-11-12 13:02:07 -0300

    Add an example to isplit
    Also compile pattern if not already.

--- src/kay/misc.py
+++ src/kay/misc.py
@@ -1,19 +1,29 @@
 """Random stuff that I don't know where to put."""
 
-
+import re
 from typing import Iterable, Pattern, Text
 
 
 def isplit(
     pattern: Pattern[Text], text: Text, include_separators: bool = False
 ) -> Iterable[Text]:
-    """
+    r"""
     Split text into parts separated by the given pattern.
 
     This yields the text before the first match, then the match, then the text after
     the match and so on. If include_separators is False (the default), the separator is
     not included in the result. In any case, empty strings are never yielded.
+
+    Examples
+    --------
+    >>> list(isplit(r'\s+', 'a b  c'))
+    ['a', 'b', 'c']
+    >>> list(isplit(r'\s+', 'a b  c', include_separators=True))
+    ['a', ' ', 'b', '  ', 'c']
     """
+    if isinstance(pattern, Text):
+        pattern = re.compile(pattern)
+
     start, end = 0, 0
     for match in pattern.finditer(text):
         # Yield the text before the match.
