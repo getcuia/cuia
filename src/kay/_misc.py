e427c41ac36e45f3eac550678e546617f7b103a8
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-11-14 19:24:15 -0300

    Move color stuff to melanin

--- src/kay/_misc.py
+++ src/kay/_misc.py
@@ -4,20 +4,7 @@ import re
 from typing import Iterable, Pattern, Text
 
 
-def clamp(value: float, min_value: float = 0.0, max_value: float = 1.0) -> float:
-    """
-    Clamp a value to a range.
 
-    Examples
-    --------
-    >>> clamp(0.5)
-    0.5
-    >>> clamp(-0.5)
-    0.0
-    >>> clamp(1.5)
-    1.0
-    """
-    return max(min_value, min(value, max_value))
 
 
 def isplit(
