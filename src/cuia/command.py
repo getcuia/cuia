fcd9d30698995e5ab9c67c3a62fb6bf697093642
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-12-03 09:41:09 -0300

    Update command.py

--- src/cuia/command.py
+++ src/cuia/command.py
@@ -1,4 +1,9 @@
-"""Application commands."""
+"""
+Application commands.
+
+Commands are ways of delegating work to the runtime environment so that things can
+run asynchronously, not blocking the main thread, and yet be easy to code and test.
+"""
 
 
 from typing import Awaitable, Callable, Optional
@@ -9,7 +14,19 @@ Command = Callable[[], Awaitable[Optional[Message]]]
 
 
 async def quit() -> Optional[Message]:
-    """Signal to quit the application."""
+    """
+    Signal a message to the application that it should quit.
+
+    Notes
+    -----
+    This conflicts with the standard `quit` function, so it is recommended to
+    import it qualified. See the example below.
+
+    Examples
+    --------
+    >>> # from cuia import quit  # Don't do this
+    >>> import cuia              # Do this instead
+    >>> cuia.quit
+    <function quit at 0x...>
+    """
     return QuitMessage()
-
-
