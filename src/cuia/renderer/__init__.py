85065ae945df17524ddf10ed3cb330e240a51ee3
Felipe S. S. Schneider <schneider.felipe@posgrad.ufsc.br>
2021-12-04 14:37:15 -0300

    Export renderers

--- src/cuia/renderer/__init__.py
+++ src/cuia/renderer/__init__.py
@@ -1,5 +1,7 @@
 """Renderer facilities."""
 
 from ._renderer import Renderer
+from .curses import CursesRenderer
+from .log import LogRenderer
 
-__all__ = ["Renderer"]
+__all__ = ["CursesRenderer", "LogRenderer", "Renderer"]
